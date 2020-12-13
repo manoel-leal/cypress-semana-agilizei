29f2b6ae802f67efe852297b79e502076a8c95ca
mlpdleal <manoellealdeveloper@gmail.com>
2020-12-13 17:22:13 -0300

    Conteúdo Terceira Aula

--- cypress/integration/compra.spec.js
+++ cypress/integration/compra.spec.js
@@ -2,6 +2,9 @@
 
 context('Compra', () => {
     it('Efetuar uma compra de produto', () => {
+       
+        cy.backgroundLogin();
+        
         cy.visit('/');
 
         let nomeProduto = 'Faded Short Sleeve T-shirts';
@@ -16,12 +19,20 @@ context('Compra', () => {
         .first()
         .click();
 
+        cy.get('.icon-ok')
+        .parent()
+        .should("contain.text", "Product successfully added to your shopping cart");
+
+        cy.get('span#layer_cart_product_title').should("contain.text", nomeProduto);
+
         cy.get(".button-container a[title='Proceed to checkout']").click();
         cy.get(".cart_navigation a[href$='order&step=1']").click();
-        cy.get("#email").type("teste123456@teste.com");
-        cy.get("#passwd").type("123456");
-        cy.get("#SubmitLogin").click();
-        cy.get("#addressesAreEquals")
+        // cy.get("#email").type("teste123456@teste.com");
+        // cy.get("#passwd").type("123456");
+        // cy.get("#SubmitLogin").click();
+       
+        cy.get("#addressesAreEquals").should('have.attr', 'checked', 'checked');
+
         cy.get("[name='processAddress']").click();
         cy.get("[name='cgv']").check();
         cy.get("[name='processCarrier']").click();
@@ -32,5 +43,18 @@ context('Compra', () => {
         .click();
 
         cy.get(".cheque-indent strong").should("contain.text", "Your order on My Store is complete.");
+
+        cy.get("div.box").invoke('text').then((text) => {
+        
+            cy.writeFile('cypress/fixtures/pedido.json', { id: `${text.match(/[A-Z][A-Z]+/g)[1] }`})
+        })
+
+        cy.get('.button-exclusive').click();
+
+        cy.readFile('cypress/fixtures/pedido.json').then((pedido) => {
+            cy.get('tr.first_item .history_link a').should('contain.text', pedido.id);
+        })
+
+        
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('backgroundLogin', () => {
+        // background login
+        cy.setCookie(
+            'PrestaShop-a30a9934ef476d11b6cc3c983616e364',
+            'R6xmma6F4U6edNQuu67M0sms02MihwK4dgUJTsAaWAwkRnYh82ybiBZ63MqEVa9VjddGP4HPOpTLAYWEsGwd4WdcWORiSPtKF9CMiRsCJ%2FfFeufCHJ8LDlQfP9yOw0Y5juOngySqVqPC9IZPkVLzaCr%2FKggRktKyQtNQ2JrCIDg1xQmnDHs1etn%2BZpTHxtF%2B9CLtfP7NiPnPt0Wjmv2CHbXGVBPOB1%2BVTMMZ1c%2Fk1l1u%2B79PFpXHv0nQVovbB4YoRMmHez2z%2FO6scdN4ZhhYcCvVq4rlzrWZ5oZk8WxFP8yKL1HmjR3J2E%2BWyxRqlN3Tn4VqK0pE09TFxDMAGUtDaKa9S5s9tu8zg62nBIpmfEZ%2F1TYdQWS3hFufOsAdYqHSq1TQb2h7m53DZPmylKu78KYRonyINwpot2Z5K1MgApH71SnxPSo8%2BePwvyVK6MOZ000324'
+        )
+})
